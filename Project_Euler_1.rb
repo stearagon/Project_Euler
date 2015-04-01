d1c26072c2e415077e7157077fe68e10a4bfc11e
stearagon <stearagon@gmail.com>
2015-04-01 16:27:57 -0400

    First commit

---
+++ Project_Euler_1.rb
@@ -0,0 +1,16 @@
+#If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+#Find the sum of all the multiples of 3 or 5 below 1000.
+
+
+def sum_of_3_5_multiples(num)
+  multiples = []
+  (0...num).each do |i|
+    if i%3==0
+      multiples << i
+    elsif i%5 ==0
+      multiples << i
+    end
+  end
+  return multiples.inject(:+)
+end
