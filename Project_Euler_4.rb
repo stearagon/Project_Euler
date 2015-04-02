f065d80edbdd1e5cf8bbaa216e92d545cdeccc5f
stearagon <stearagon@gmail.com>
2015-04-01 23:01:50 -0400

    first commit

--- Project_Euler_4.rb
+++ Project_Euler_4.rb
@@ -1,14 +1,21 @@
-palindromes = []
+palindromes = 0
 
-i = 100
-while i < 1000
-  b = 100
-  while b < 1000
-  x = i * b
-  palindromes << x if x.to_s.reverse == x.to_s
-  b += 1
+i = 999
+while i >= 100
+  if i % 11 == 0
+    delta = 1
+    b = 999
+  else
+    delta = 11
+    b = 990
   end
-  i += 1
+  while b >= i
+      x = i * b
+      break if x < palindromes
+      palindromes = x if x.to_s.reverse == x.to_s && x > palindromes
+      b -= delta
+  end
+  i -= 1
 end
 
-puts palindromes.max
+puts palindromes
