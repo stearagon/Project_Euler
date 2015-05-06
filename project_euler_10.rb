57d4e50c4c20b6d2ca6de3cd2bd26a0d0346f8b7
stearagon <stearagon@gmail.com>
2015-05-06 19:37:33 -0400

    Sped up Project Euler 10 to ~3 secs

--- project_euler_10.rb
+++ project_euler_10.rb
@@ -1,16 +1,29 @@
+x = Time.now
+
 def is_prime?(n)
-  return false if n == 1 || n == 0
-  (2..Math.sqrt(n)).each do |i|
-    return false if n % i == 0
+  return false if n == 1
+  return true if n < 4
+  return false if n % 2 == 0
+  return true if n < 9
+  return false if n % 3 == 0
+  r = Math.sqrt(n)
+  f = 5
+  while f <= r
+    return false if n % f == 0
+    return false if n % (f+2) == 0
+    f+=6
   end
   return true
 end
 
-answer = 0
-i = 1
+i = 5
+sum = 5
 while i < 2000000
-  answer += i if is_prime?(i)
-  i+=1
+  sum += i if is_prime?(i)
+  i+=2
 end
 
-puts answer
+puts sum
+
+
+puts Time.now - x
